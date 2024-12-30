0ed45907317eb38e88edd0dcd73eb9b93ef4ea84
“Gordon <gwdina@gmail.com>
2024-12-30 16:38:46 -0600

    removed unnecessary files, BoardSquares now will highlight movable squares in yellow and highlight unmovable squares as red if knight it brought over

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-// import ReactDOM from 'react-dom'
-import Knight from './Knight'
-
-function App() {
-  
-  }
-
-export default App;
--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -6,13 +6,35 @@ import { useDrop } from 'react-dnd'
 
 function BoardSquare({ x, y, children }) {
   const black = (x + y) % 2 === 1
-  const [{ isOver }, drop] = useDrop(() => ({
-    accept: ItemTypes.KNIGHT,
-    drop: () => moveKnight(x, y),
-    collect: monitor => ({
-      isOver: !!monitor.isOver(),
+  const [{ isOver, canDrop }, drop] = useDrop(
+    () => ({
+      accept: ItemTypes.KNIGHT,
+      drop: () => moveKnight(x, y),
+      canDrop: () => canMoveKnight(x, y),
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+        canDrop: !!monitor.canDrop()
+      })
     }),
-  }), [x, y])
+    [x, y]
+  )
+
+  function Overlay({ color }) {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          height: '100%',
+          width: '100%',
+          zIndex: 1,
+          opacity: 0.5,
+          backgroundColor: color,
+        }}
+      />
+    )
+  }
 
   return (
     <div
@@ -20,24 +42,13 @@ function BoardSquare({ x, y, children }) {
       style={{
         position: 'relative',
         width: '100%',
-        height: '100%',
+        height: '100%'
       }}
     >
       <Square black={black}>{children}</Square>
-      {isOver && (
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '100%',
-            width: '100%',
-            zIndex: 1,
-            opacity: 0.5,
-            backgroundColor: 'yellow',
-          }}
-        />
-      )}
+      {isOver && !canDrop && <Overlay color="red" />}
+      {!isOver && canDrop && <Overlay color="yellow" />}
+      {isOver && canDrop && <Overlay color="green" />}
     </div>
   )
 }
