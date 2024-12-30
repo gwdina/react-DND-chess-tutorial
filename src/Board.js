2e135e7a13e04b33313d9e58e0cee1192d2f7778
“Gordon <gwdina@gmail.com>
2024-12-30 16:49:14 -0600

    removed some commented out code

--- src/Board.js
+++ src/Board.js
@@ -25,25 +25,7 @@ function renderPiece(x, y, [knightX, knightY]) {
     }
   }
 
-// function renderSquare(i, [knightX, knightY]) {
-//   const x = i % 8
-//   const y = Math.floor(i / 8)
-//   const isKnightHere = x === knightX && y === knightY
-//   const black = (x + y) % 2 === 1
-//   const piece = isKnightHere ? <Knight /> : null
 
-//   return (
-//     <div key={i} style={{ width: '12.5%', height: '12.5%' }} onClick={() => handleSquareClick(x, y)}>
-//       <Square black={black}>{piece}</Square>
-//     </div>
-//   )
-// }
-
-// function handleSquareClick(toX, toY) {
-//     if (canMoveKnight(toX, toY)) {
-//       moveKnight(toX, toY)
-//     }
-//   }
 
 export default function Board({ knightPosition }) {
   const squares = []
